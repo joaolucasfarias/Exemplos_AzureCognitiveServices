cce4895fe04bf8ece16b2611cc5d6624a5edb6e0
João Lucas Farias <joao.l.farias@hotmail.com>
2020-02-21 10:11:34 -0300

    Primeira versão de exemplo

---
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/Face.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AzureCognitiveServices
+{
+    public class FaceModel : PageModel
+    {
+        public ICollection<string> Mensagens { get; private set; }
+
+
+        private IWebHostEnvironment _environment;
+        public FaceModel(IWebHostEnvironment environment) =>
+            _environment = environment;
+
+        [BindProperty]
+        public IFormFile Arquivo { get; set; }
+
+        public void OnPost()
+        {
+            if (Arquivo is null)
+            {
+                var url = Request.Form["url"];
+                Mensagens = new Face.DeteccaoFacial().DetectarRostosPorUrl(url).Result;
+            }
+            else
+            {
+                var pasta = Path.Combine(_environment.ContentRootPath, "imagens");
+                if (!Directory.Exists(pasta))
+                    Directory.CreateDirectory(pasta);
+
+                var caminhoDoArquivo = Path.Combine(_environment.ContentRootPath, "imagens", Arquivo.FileName);
+
+                using (var fileStream = new FileStream(caminhoDoArquivo, FileMode.Create))
+                    Arquivo.CopyTo(fileStream);
+
+                Mensagens = new Face.DeteccaoFacial().DetectarRostosPorArquivo(caminhoDoArquivo).Result;
+
+                Directory.Delete(pasta, true);
+            }
+        }
+    }
+}
---
+++ src/AzureCognitiveServices/Face/DeteccaoFacial.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Face
+{
+    public class DeteccaoFacial
+    {
+        private const string _chave = "<sua chave>";
+        private const string _endpoint = "<seu endpoint>";
+
+        private IFaceClient _servicoCongnitivoDeFace;
+
+        public DeteccaoFacial() =>
+            _servicoCongnitivoDeFace = new FaceClient(new ApiKeyServiceClientCredentials(_chave)) { Endpoint = _endpoint };
+
+        public async Task<ICollection<string>> DetectarRostosPorUrl(string url)
+        {
+            var rostosDetectados = await _servicoCongnitivoDeFace.Face.DetectWithUrlAsync(url,
+                    returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.Age, FaceAttributeType.Emotion,
+                FaceAttributeType.Gender, FaceAttributeType.Smile });
+
+            return RetornarInformacoesDosRostosDetectados(rostosDetectados);
+        }
+
+        public async Task<ICollection<string>> DetectarRostosPorArquivo(string localDoArquivo)
+        {
+            var arquivo = new FileStream(localDoArquivo, FileMode.Open);
+
+            var rostosDetectados = await _servicoCongnitivoDeFace.Face.DetectWithStreamAsync(arquivo,
+                returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.Age, FaceAttributeType.Emotion,
+                FaceAttributeType.Gender, FaceAttributeType.Smile });
+
+            return RetornarInformacoesDosRostosDetectados(rostosDetectados);
+        }
+
+        private static ICollection<string> RetornarInformacoesDosRostosDetectados(IList<DetectedFace> rostosDetectados)
+        {
+            if (rostosDetectados.Count == 0)
+                return new List<string> { "Nenhum rosto encontrado na imagem." };
+
+            var informacoesDeRetorno = new List<string>(rostosDetectados.Count)
+            {$"Foram encontrados {rostosDetectados.Count} rosto(s) na imagem:" };
+
+            var posicaoDoRosto = 0;
+
+            foreach (var rosto in rostosDetectados)
+            {
+                var informacoesDoRosto = $"{++posicaoDoRosto}º - Aparenta ter {rosto.FaceAttributes.Age} anos, ";
+
+                var tipoDeEmocao = string.Empty;
+                var porcentagemDaEmocao = 0.0;
+                var emocao = rosto.FaceAttributes.Emotion;
+                if (emocao.Anger > porcentagemDaEmocao) { porcentagemDaEmocao = emocao.Anger; tipoDeEmocao = "com raiva"; }
+                if (emocao.Contempt > porcentagemDaEmocao) { porcentagemDaEmocao = emocao.Contempt; tipoDeEmocao = "com desprezo"; }
+                if (emocao.Disgust > porcentagemDaEmocao) { porcentagemDaEmocao = emocao.Disgust; tipoDeEmocao = "com nojo"; }
+                if (emocao.Fear > porcentagemDaEmocao) { porcentagemDaEmocao = emocao.Fear; tipoDeEmocao = "com medo"; }
+                if (emocao.Happiness > porcentagemDaEmocao) { porcentagemDaEmocao = emocao.Happiness; tipoDeEmocao = "feliz"; }
+                if (emocao.Neutral > porcentagemDaEmocao) { porcentagemDaEmocao = emocao.Neutral; tipoDeEmocao = "neutro"; }
+                if (emocao.Sadness > porcentagemDaEmocao) { porcentagemDaEmocao = emocao.Sadness; tipoDeEmocao = "triste"; }
+                if (emocao.Surprise > porcentagemDaEmocao) { tipoDeEmocao = "surpreso"; }
+
+                var possivelGenero = "female".Equals(rosto.FaceAttributes.Gender.ToString().ToLower())
+                    ? "uma mulher"
+                    : "um homem";
+
+                informacoesDoRosto += $"deve estar {tipoDeEmocao}, baseado na aparência, há a possibilidade de ser {possivelGenero}, porcentagem de sorriso é de: {rosto.FaceAttributes.Smile * 100}%";
+
+                informacoesDeRetorno.Add(informacoesDoRosto);
+            }
+
+            return informacoesDeRetorno;
+        }
+    }
+}
