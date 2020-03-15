08a961ad8287b9069209458d1e557fc6aaa703ab
João Lucas Farias <joao.l.farias@hotmail.com>
2020-03-15 19:33:29 -0300

    Criando a base do método OnPost para capturar requisições

--- src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/ComputerVision.cshtml.cs
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/ComputerVision.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
+using System.IO;
 
 namespace AzureCognitiveServices
 {
@@ -13,11 +14,43 @@ namespace AzureCognitiveServices
         public string AnaliseEscolhida { get; private set; }
 
         private IWebHostEnvironment _environment;
+        private bool _analisarPorUrl;
 
         [BindProperty]
         public IFormFile Arquivo { get; set; }
 
         public ComputerVisionModel(IWebHostEnvironment environment) =>
             _environment = environment;
+
+        public void OnPost()
+        {
+            var imagem = string.Empty;
+
+            if (Arquivo is null)
+            {
+                imagem = Request.Form["url"];
+                _analisarPorUrl = true;
+            }
+            else
+            {
+                var pasta = Path.Combine(_environment.ContentRootPath, "imagens");
+                if (!Directory.Exists(pasta))
+                    Directory.CreateDirectory(pasta);
+
+                var caminhoDoArquivo = Path.Combine(_environment.ContentRootPath, "imagens", Arquivo.FileName);
+
+                using (var fileStream = new FileStream(caminhoDoArquivo, FileMode.Create))
+                    Arquivo.CopyTo(fileStream);
+
+                _analisarPorUrl = false;
+
+                imagem = caminhoDoArquivo;
+
+                Directory.Delete(pasta, true);
+            }
+
+            if (!_analisarPorUrl)
+                Directory.Delete(pasta, true);
+        }
     }
 }
