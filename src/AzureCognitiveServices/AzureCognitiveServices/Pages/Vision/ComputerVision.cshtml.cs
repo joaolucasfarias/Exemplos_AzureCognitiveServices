48ae50664e52e1091709b093c9230c442650e335
João Lucas Farias <joao.l.farias@hotmail.com>
2020-03-15 19:20:23 -0300

    Modificando layout principal para abrir as modais, melhorando o HTML e criando a modal

--- src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/ComputerVision.cshtml.cs
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/ComputerVision.cshtml.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace AzureCognitiveServices
 {
     public class ComputerVisionModel : PageModel
     {
+        public ICollection<string> Mensagens { get; private set; }
+
+        public string AnaliseEscolhida { get; private set; }
+
+        private IWebHostEnvironment _environment;
 
         [BindProperty]
         public IFormFile Arquivo { get; set; }
 
-        public void OnGet()
-        {
-            var aaa = new ComputerVision.AnaliseDeImagem().AnalisarPorUrl("https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png").Result;
-            //var aaa = new ComputerVision.AnaliseDeImagem().AnalisarPorUrl("https://www.biography.com/.image/t_share/MTE1ODA0OTcxMjkwNTYwMDEz/michael-jackson-pepsi-commercial-raw.jpg").Result;
-            //var aaa = new ComputerVision.AnaliseDeImagem().AnalisarPorUrl("https://gitcdn.xyz/cdn/Tony607/blog_statics/da023c6fa09cc1c6de8ce8ab9358f74f21f56579/images/landmark/eiffel-tower.jpg").Result;
-            // var aaa = new ComputerVision.AnaliseDeImagem().AnalisarPorUrl("https://i.ytimg.com/vi/xWlS57ypHWw/hqdefault.jpg").Result;
-            new ComputerVision.AnaliseDeImagem().RetornarLegenda(aaa);
-
-        }
+        public ComputerVisionModel(IWebHostEnvironment environment) =>
+            _environment = environment;
     }
 }
