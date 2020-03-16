0a1b3cf7de750f35e012e92d24113c666c8c1e35
João Lucas Farias <joao.l.farias@hotmail.com>
2020-03-16 09:00:47 -0300

    Criando os métodos que chamam as funções corretas

--- src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/ComputerVision.cshtml.cs
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/ComputerVision.cshtml.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using ComputerVision;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 using System.Collections.Generic;
 using System.IO;
 
@@ -24,7 +26,8 @@ namespace AzureCognitiveServices
 
         public void OnPost()
         {
-            var imagem = string.Empty;
+            string imagem;
+            var pasta = string.Empty;
 
             if (Arquivo is null)
             {
@@ -33,7 +36,7 @@ namespace AzureCognitiveServices
             }
             else
             {
-                var pasta = Path.Combine(_environment.ContentRootPath, "imagens");
+                pasta = Path.Combine(_environment.ContentRootPath, "imagens");
                 if (!Directory.Exists(pasta))
                     Directory.CreateDirectory(pasta);
 
@@ -47,8 +50,116 @@ namespace AzureCognitiveServices
                 imagem = caminhoDoArquivo;
             }
 
+            AnalisarImagem(imagem);
+
             if (!_analisarPorUrl)
-                Directory.Delete(imagem, true);
+                Directory.Delete(pasta, true);
+        }
+
+        private void AnalisarImagem(string imagem)
+        {
+            var analiseDeImagem = new AnaliseDeImagem();
+
+            var analiseRealizada = _analisarPorUrl
+                ? analiseDeImagem.AnalisarPorUrl(imagem).Result
+                : analiseDeImagem.AnalisarPorArquivo(imagem).Result;
+
+            Mensagens = new List<string>();
+
+            AnalisarLegenda(analiseDeImagem, analiseRealizada);
+            AnalisarCategorias(analiseDeImagem, analiseRealizada);
+            AnalisarTags(analiseDeImagem, analiseRealizada);
+            AnalisarLogos(analiseDeImagem, analiseRealizada);
+            AnalisarRostos(analiseDeImagem, analiseRealizada);
+            AnalisarConteudoAdultoOuSensivel(analiseDeImagem, analiseRealizada);
+            AnalisarEsquemaDeCores(analiseDeImagem, analiseRealizada);
+            AnalisarCelebridades(analiseDeImagem, analiseRealizada);
+            AnalisarPontosDeReferencia(analiseDeImagem, analiseRealizada);
+        }
+
+        private void AnalisarLegenda(AnaliseDeImagem analiseDeImagem, ImageAnalysis analiseRealizada)
+        {
+            if (!AnaliseEscolhidaFoi("legenda", "Descrição"))
+                return;
+
+            Mensagens.Add(analiseDeImagem.RetornarLegenda(analiseRealizada));
+        }
+
+        private void AnalisarCategorias(AnaliseDeImagem analiseDeImagem, ImageAnalysis analiseRealizada)
+        {
+            if (!AnaliseEscolhidaFoi("categoria", "Categorias"))
+                return;
+
+            Mensagens.Add(analiseDeImagem.RetornarCategorias(analiseRealizada));
+        }
+
+        private void AnalisarTags(AnaliseDeImagem analiseDeImagem, ImageAnalysis analiseRealizada)
+        {
+            if (!AnaliseEscolhidaFoi("tags", "Tags"))
+                return;
+
+            Mensagens.Add(analiseDeImagem.RetornarTags(analiseRealizada));
+        }
+
+        private void AnalisarLogos(AnaliseDeImagem analiseDeImagem, ImageAnalysis analiseRealizada)
+        {
+            if (!AnaliseEscolhidaFoi("logos", "Logos"))
+                return;
+
+            Mensagens.Add(analiseDeImagem.RetornarLogos(analiseRealizada));
+        }
+
+        private void AnalisarRostos(AnaliseDeImagem analiseDeImagem, ImageAnalysis analiseRealizada)
+        {
+            if (!AnaliseEscolhidaFoi("rostos", "Rostos"))
+                return;
+
+            Mensagens.Add(analiseDeImagem.RetornarRostos(analiseRealizada));
+        }
+
+        private void AnalisarConteudoAdultoOuSensivel(AnaliseDeImagem analiseDeImagem, ImageAnalysis analiseRealizada)
+        {
+            if (!AnaliseEscolhidaFoi("adulto", "Conteúdo adulto ou sensível"))
+                return;
+
+            Mensagens.Add(analiseDeImagem.RetornarConteudoAdultoOuSensivel(analiseRealizada));
+        }
+
+        private void AnalisarEsquemaDeCores(AnaliseDeImagem analiseDeImagem, ImageAnalysis analiseRealizada)
+        {
+            if (!AnaliseEscolhidaFoi("cores", "Esquema de cores"))
+                return;
+
+            Mensagens.Add(analiseDeImagem.RetornarEsquemaDeCores(analiseRealizada));
+        }
+
+        private void AnalisarCelebridades(AnaliseDeImagem analiseDeImagem, ImageAnalysis analiseRealizada)
+        {
+            if (!AnaliseEscolhidaFoi("celebridades", "Celebridades"))
+                return;
+
+            Mensagens.Add(analiseDeImagem.RetornarCelebridades(analiseRealizada));
+        }
+
+        private void AnalisarPontosDeReferencia(AnaliseDeImagem analiseDeImagem, ImageAnalysis analiseRealizada)
+        {
+            if (!AnaliseEscolhidaFoi("landmark", "Pontos de referência"))
+                return;
+
+            Mensagens.Add(analiseDeImagem.RetornarPontosDeReferencia(analiseRealizada));
+        }
+
+        private bool AnaliseEscolhidaFoi(string analiseEscolhida, string tituloDaAnaliseEscolhida)
+        {
+            if (!analiseEscolhida.Equals(Request.Form["analiseEscolhida"])
+                && !"tudo".Equals(Request.Form["analiseEscolhida"]))
+                return false;
+
+            AnaliseEscolhida = "tudo".Equals(Request.Form["analiseEscolhida"])
+                ? "Analise da imagem por completo"
+                : tituloDaAnaliseEscolhida;
+
+            return true;
         }
     }
 }
