5135a7d130e229d7ab581721aae870e61ce558f9
João Lucas Farias <joao.l.farias@hotmail.com>
2020-04-05 22:58:06 -0300

    Primeira versão do C# já cadastrando os projetos

--- src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/CustomVision/Projetos.cshtml.cs
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/CustomVision/Projetos.cshtml.cs
@@ -17,13 +17,27 @@ namespace AzureCognitiveServices
         public ProjetosModel() =>
             _treinamento = new Treinamento();
 
-        public void OnGet()
+        public void OnGet() =>
+            RecuperarProjetos();
+
+        public IEnumerable<Tag> ListarTags(Project projeto) =>
+            _treinamento.ListarTags(projeto);
+
+        public void OnPost()
+        {
+            var tipo = Request.Form["tipo"];
+            var nome = Request.Form["nome"];
+            var descricao = Request.Form["descricao"];
+
+            _treinamento.CriarProjeto(nome, descricao);
+
+            RecuperarProjetos();
+        }
+
+        private void RecuperarProjetos()
         {
             Projetos = _treinamento.ListarProjetos();
             PodeAdicionarNovosProjetos = Projetos.Count() < 2;
         }
-
-        public IEnumerable<Tag> ListarTags(Project projeto) =>
-            _treinamento.ListarTags(projeto);
     }
 }
