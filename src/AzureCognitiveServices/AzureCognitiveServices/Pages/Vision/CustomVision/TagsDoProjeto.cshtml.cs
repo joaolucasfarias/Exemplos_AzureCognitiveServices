f88f69e506be0ca4d758771c461dd8a5bd85044c
João Lucas Farias <joao.l.farias@hotmail.com>
2020-04-10 11:07:31 -0300

    Código e HTML para listar e excluir as tags

--- src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/CustomVision/TagsDoProjeto.cshtml.cs
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/CustomVision/TagsDoProjeto.cshtml.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CustomVision;
-using Microsoft.AspNetCore.Mvc;
+﻿using CustomVision;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AzureCognitiveServices
 {
@@ -15,11 +12,31 @@ namespace AzureCognitiveServices
 
         public Project Projeto { get; set; }
 
-        public IEnumerable<Tag> Tags { get; set; }
+        public IList<Tag> Tags { get; set; }
 
-        public void OnGet()
+        public TagsDoProjetoModel() =>
+            _treinamento = new Treinamento();
+
+        public void OnGet(string idDoProjeto) =>
+            CarregarInformacoes(idDoProjeto);
+
+        private void CarregarInformacoes(string idDoProjeto)
         {
+            CarregarProjeto(idDoProjeto);
+            ListarTags(Projeto);
+        }
 
+        private void CarregarProjeto(string idDoProjeto) =>
+            Projeto = _treinamento.CarregarProjeto(idDoProjeto);
+
+        private void ListarTags(Project projeto) =>
+            Tags = _treinamento.ListarTags(Projeto).ToList();
+
+        public void OnPost(string idDoProjeto)
+        {
+            CarregarProjeto(idDoProjeto);
+            _treinamento.ExcluirTag(Projeto, Request.Form["idDaTag"]);
+            ListarTags(Projeto);
         }
     }
 }
