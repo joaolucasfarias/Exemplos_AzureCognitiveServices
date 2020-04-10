4c552eef121a2476af52b7dfea6e1c2b55536b31
João Lucas Farias <joao.l.farias@hotmail.com>
2020-04-10 10:16:17 -0300

    Página para criar as tags do projeto

---
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/CustomVision/TagsDoProjeto.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzureCognitiveServices
+{
+    public class TagsDoProjetoModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
