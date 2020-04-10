ec0a133186116d612018f71973910dafdb7b39fa
João Lucas Farias <joao.l.farias@hotmail.com>
2020-04-10 09:38:22 -0300

    Código para salvar um novo projeto

--- src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/CustomVision/SalvarProjeto.cshtml.cs
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/CustomVision/SalvarProjeto.cshtml.cs
@@ -2,16 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CustomVision;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training.Models;
 
 namespace AzureCognitiveServices
 {
     public class SalvarProjetoModel : PageModel
     {
-        public void OnGet()
+        private readonly Treinamento _treinamento;
+
+        public Project Projeto { get; set; }
+
+        public SalvarProjetoModel()
+        {
+            _treinamento = new Treinamento();
+        }
+
+        public void OnGet(string idDoProjeto)
         {
 
+        }
+
+        public void OnPost()
+        {
+            var nome = Request.Form["nome"];
+            var descricao = Request.Form["descricao"];
+            _treinamento.CriarProjeto(nome, descricao);
+            RedirectToPage("/Vision/CustomVision/Projetos");
+        }
+
+        private void CarregarProjeto(string idDoProjeto)
+        {
+            Projeto = _treinamento.CarregarProjeto(idDoProjeto);
         }
     }
 }
