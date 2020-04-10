e194a331e1b686267a644160dea0a54c49d2098d
João Lucas Farias <joao.l.farias@hotmail.com>
2020-04-10 10:40:36 -0300

    HTML e código primário para as tags

--- src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/CustomVision/TagsDoProjeto.cshtml.cs
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/CustomVision/TagsDoProjeto.cshtml.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CustomVision;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training.Models;
 
 namespace AzureCognitiveServices
 {
     public class TagsDoProjetoModel : PageModel
     {
+        private readonly Treinamento _treinamento;
+
+        public Project Projeto { get; set; }
+
+        public IEnumerable<Tag> Tags { get; set; }
+
         public void OnGet()
         {
 
