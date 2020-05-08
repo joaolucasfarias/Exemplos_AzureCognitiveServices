08962e8415baa2b0e7a5b5541ae7e275422f61f1
João Lucas Farias <joao.l.farias@hotmail.com>
2020-05-08 11:14:29 -0300

    Página para conter a predição pro usuário

---
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Vision/CustomVision/Predicao.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomVision;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzureCognitiveServices
+{
+    public class PredicaoModel : PageModel
+    {
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
