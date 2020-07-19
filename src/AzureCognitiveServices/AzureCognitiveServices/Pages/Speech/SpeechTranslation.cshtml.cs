5cd1bb416f0572dc4e174c7b8664f939515d589b
João Lucas Farias <joao.l.farias@hotmail.com>
2020-07-19 17:17:27 -0300

    Adicionando algumas melhorias

--- src/AzureCognitiveServices/AzureCognitiveServices/Pages/Speech/SpeechTranslation.cshtml.cs
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Speech/SpeechTranslation.cshtml.cs
@@ -8,21 +8,27 @@ namespace AzureCognitiveServices
 {
     public class SpeechTranslationModel : PageModel
     {
-        public ICollection<string> Mensagens { get; private set; }
+        public ICollection<string> Mensagens { get; private set; } = new List<string>(0);
 
         public readonly IEnumerable<Idioma> Idiomas;
 
         [BindProperty]
-        public IList<string> IdiomasAlvoDeTraducao { get; set; }
+        public IList<string> TraduzirPara { get; set; }
 
         public SpeechTranslationModel() =>
             Idiomas = Idioma.IdiomasDisponiveis();
 
         public void OnPost()
         {
+            if (!TraduzirPara.Any())
+            {
+                Mensagens.Add("Não foi possível traduzir. Você precisa selecionar pelo menos 1 idioma alvo.");
+                return;
+            }
+
             var traduzirDe = Idiomas.FirstOrDefault(n => Request.Form["traduzirDe"].Equals(n.Codigo));
 
-            var traduzirPara = Idiomas.Where(i => IdiomasAlvoDeTraducao.Any(alvo => alvo.Equals(i)));
+            var traduzirPara = Idiomas.Where(i => TraduzirPara.Any(alvo => alvo.Equals(i.Codigo)));
 
             Mensagens = new TraducaoDeFala().Ouvir(traduzirDe, traduzirPara).Result;
         }
