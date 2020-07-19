e54c34ccc2b162f145f92149a3493e219812611f
João Lucas Farias <joao.l.farias@hotmail.com>
2020-07-19 16:51:24 -0300

    Código do servidor finalizado

--- src/AzureCognitiveServices/AzureCognitiveServices/Pages/Speech/SpeechTranslation.cshtml.cs
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Speech/SpeechTranslation.cshtml.cs
@@ -1,17 +1,30 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SpeechTranslation;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AzureCognitiveServices
 {
     public class SpeechTranslationModel : PageModel
     {
-        public void OnGet()
-        {
+        public ICollection<string> Mensagens { get; private set; }
 
+        public readonly IEnumerable<Idioma> Idiomas;
+
+        [BindProperty]
+        public IList<string> IdiomasAlvoDeTraducao { get; set; }
+
+        public SpeechTranslationModel() =>
+            Idiomas = Idioma.IdiomasDisponiveis();
+
+        public void OnPost()
+        {
+            var traduzirDe = Idiomas.FirstOrDefault(n => Request.Form["traduzirDe"].Equals(n.Codigo));
+
+            var traduzirPara = Idiomas.Where(i => IdiomasAlvoDeTraducao.Any(alvo => alvo.Equals(i)));
+
+            Mensagens = new TraducaoDeFala().Ouvir(traduzirDe, traduzirPara).Result;
         }
     }
 }
