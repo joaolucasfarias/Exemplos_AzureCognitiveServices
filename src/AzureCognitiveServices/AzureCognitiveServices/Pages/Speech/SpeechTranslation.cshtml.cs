af1103277208d462fd5f96952dc06ede0f6064fd
João Lucas Farias <joao.l.farias@hotmail.com>
2020-07-19 16:40:19 -0300

    Página para chamar os métodos criados

---
+++ src/AzureCognitiveServices/AzureCognitiveServices/Pages/Speech/SpeechTranslation.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzureCognitiveServices
+{
+    public class SpeechTranslationModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
